f4cd77d8ad18b14fb9681ece94394ce8ee9ca34e
Isak Lindbeck <isak.lindbeck@atrazeneca.com>
2023-12-09 19:06:16 +0100

    Day 9

--- aoc-2023/src/day09.rs
+++ aoc-2023/src/day09.rs
@@ -5,30 +5,19 @@ use crate::util::split_parse;
 pub fn run(input: String) -> (usize, usize) {
     let sequences: Vec<Vec<i64>> = input.lines().map(|line| split_parse(" ", line)).collect();
 
-    let ans_1: i64 = sequences.iter()
-        .map(calculate_forwards)
-        .sum();
-
-
-    let ans_2: i64 = sequences.iter()
-        .map(calculate_backwards)
-        .sum();
+    let (ans_2, ans_1): (i64, i64) = sequences.iter()
+        .map(calculate)
+        .reduce(|acc, new| (acc.0 + new.0, acc.1 + new.1))
+        .unwrap();
 
     (ans_1 as usize, ans_2 as usize)
 }
 
-fn calculate_forwards(v: &Vec<i64>) -> i64 {
+fn calculate(v: &Vec<i64>) -> (i64, i64) {
     let differences: Vec<i64> = v.iter().tuple_windows().map(|(a, b)| b - a).collect();
     if differences.iter().all_equal() {
-        return v[v.len() - 1] + differences[0];
+        return (v[0] - differences[0], v[v.len() - 1] + differences[0]);
     }
-    v[v.len() - 1] + calculate_forwards(&differences)
-}
-
-fn calculate_backwards(v: &Vec<i64>) -> i64 {
-    let differences: Vec<i64> = v.iter().tuple_windows().map(|(a, b)| b - a).collect();
-    if differences.iter().all_equal() {
-        return v[0] - differences[0];
-    }
-    v[0] - calculate_backwards(&differences)
+    let res = calculate(&differences);
+    (v[0] - res.0, v[v.len() - 1] + res.1)
 }
