7c23e079478a2327681d93e663e39528f659a598
Isak Lindbeck <isak.lindbeck@atrazeneca.com>
2023-12-11 19:11:25 +0100

    Day 11 cleanup

--- aoc-2023/src/day11.rs
+++ aoc-2023/src/day11.rs
@@ -4,58 +4,41 @@ use std::collections::HashSet;
 use itertools::Itertools;
 
 pub fn run(input: String) -> (usize, usize) {
+    let length = input.lines().next().unwrap().len();
 
-    let line_len = input.lines().next().unwrap().len();
-    let height = input.lines().count();
-    let mut map: Vec<Vec<char>> = vec![vec!['.'; height]; line_len];
+    let mut galaxies: HashSet<(usize, usize)> = HashSet::new();
+    let mut empty_x: HashSet<usize> = (0..length).collect();
+    let mut empty_y: HashSet<usize> = (0..length).collect();
+
     input.lines().enumerate().for_each(|(y, line)| {
         line.chars().enumerate().for_each(|(x, c)| {
             if c == '#' {
-                map[x][y] = '#';
+                galaxies.insert((x, y));
+                empty_x.remove(&x);
+                empty_y.remove(&y);
             }
         })
     });
 
-    let mut empty_x: HashSet<usize> = HashSet::new();
-    let mut empty_y: HashSet<usize> = HashSet::new();
-    let empty_row = ".".repeat(line_len);
-    input.lines().enumerate().filter(|(_, line)| empty_row.eq(line)).for_each(|(y, _)| {
-       empty_y.insert(y);
-    });
-    map.iter().enumerate().filter(|(_, v)| v.iter().all_equal()).for_each(|(x, _)| {
-        empty_x.insert(x);
-    });
+    let mut ans_1 = 0;
+    let mut ans_2 = 0;
+    for v in galaxies.iter().combinations(2) {
+        let min_x = cmp::min(v[0].0, v[1].0);
+        let max_x = cmp::max(v[0].0, v[1].0);
+        let min_y = cmp::min(v[0].1, v[1].1);
+        let max_y = cmp::max(v[0].1, v[1].1);
 
-    let mut galaxies: HashSet<(usize, usize)> = HashSet::new();
-    for x in 0..map.len() {
-        for y in 0..map[x].len() {
-            print!("{}", map[x][y]);
-            if map[x][y] == '#' {
-                galaxies.insert((x, y));
-            }
-        }
-        println!()
+        let x_range = min_x..max_x;
+        let y_range = min_y..max_y;
+
+        let passed_empty_x = empty_x.iter().filter(|x| x_range.contains(x)).count();
+        let passed_empty_y = empty_y.iter().filter(|y| y_range.contains(y)).count();
+        let empty_distance = passed_empty_x + passed_empty_y;
+
+        let distance = x_range.count() + y_range.count();
+        ans_1 += distance + empty_distance;
+        ans_2 += distance + empty_distance * (1_000_000 - 1);
     }
 
-    println!("empty x: {:?}", empty_x);
-    println!("empty y: {:?}", empty_y);
-
-    let ans_1: usize = galaxies.iter().combinations(2).map(|v| {
-        let x1 = v[0].0;
-        let y1 = v[0].1;
-        let x2 = v[1].0;
-        let y2 = v[1].1;
-
-        let empty_distance = 1_000_000 - 1;
-        let passed_empty_x = empty_x.iter().filter(|x| (cmp::min(x1,x2)..cmp::max(x1,x2)).contains(x)).count() * empty_distance;
-        let passed_empty_y = empty_y.iter().filter(|y| (cmp::min(y1,y2)..cmp::max(y1,y2)).contains(y)).count() * empty_distance;
-        let empty_distance = passed_empty_x + passed_empty_y;
-        let distance = x1.abs_diff(x2) + y1.abs_diff(y2) + empty_distance;
-        println!("({} {}) -> ({} {}) + {}x + {}y == {}", x1, y1, x2, y2, passed_empty_x, passed_empty_y, distance);
-
-
-        distance
-    }).sum();
-
-    (ans_1, 0)
+    (ans_1, ans_2)
 }
