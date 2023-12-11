e5cb8eef43d5905e72cac5ff48a90b9dbf18906d
Isak Lindbeck <isak.lindbeck@atrazeneca.com>
2023-12-12 00:08:03 +0100

    Day 10 cleanup

--- aoc-2023/src/day10.rs
+++ aoc-2023/src/day10.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 pub fn run(input: String) -> (usize, usize) {
     let input = input;
 
@@ -12,73 +10,62 @@ pub fn run(input: String) -> (usize, usize) {
         line.chars().enumerate().for_each(|(x, c)| map[x][len - 1 - y] = c);
     });
 
-    let start: Coord = map.iter().enumerate().find_map(|(x, line)| {
-        line.iter().enumerate().find_map(|(y, pipe)| {
-            if *pipe == 'S' {
-                Some(Coord { x: x as i32, y: y as i32 })
-            } else {
-                None
-            }
-        })
-    }).unwrap();
-
-    let left = map[(start.x - 1) as usize][start.y as usize];
-    let right = map[(start.x + 1) as usize][start.y as usize];
-    let up = map[start.x as usize][(start.y + 1) as usize];
-    let left: bool = left == '─' || left == '└' || left == '┌';
-    let right: bool = right == '─' || right == '┐' || right == '┘';
-    let up: bool = up == '│' || up == '┐' || up == '┘';
-
-    let start_char = match (left, right, up) {
-        (false, true, false) => '┌',
-        (false, true, true) => '└',
-        (true, true, false) => '─',
-        (true, false, false) => '┐',
-        (true, false, true) => '┘',
-        (false, false, true) => '│',
-        _ => panic!("Unknown start char"),
-    };
-
+    let start: Coord = find_start_coord(&mut map);
+    let start_char = get_start_tile_type(&map, &start);
     map[start.x as usize][start.y as usize] = start_char;
+
+    let mut part_of_loop: Vec<Vec<bool>> = vec![vec![false; len]; len];
+    part_of_loop[start.x as usize][start.y as usize] = true;
+
     let mut prev = start;
-    let mut current = match start_char {
-        '┌' | '└' | '─' => Coord { x: start.x + 1, y: start.y },
-        '┐' | '┘' => Coord { x: start.x - 1, y: start.y },
-        _ => Coord { x: start.x, y: start.y + 1 },
-    };
+    let mut current = get_first_step(start, start_char);
 
     let mut ans_1: usize = 1;
-    let mut part_of_loop: HashSet<(i32, i32)> = HashSet::new();
-    part_of_loop.insert((start.x, start.y));
     while current != start {
-        part_of_loop.insert((current.x, current.y));
+        part_of_loop[current.x as usize][current.y as usize] = true;
         ans_1 += 1;
-        let p = &map[current.x as usize][current.y as usize];
 
         let dx: i32 = current.x - prev.x;
         let dy: i32 = current.y - prev.y;
-        let next = match p {
-            '┌' => Coord { x: current.x + dy, y: current.y + dx },
-            '└' => Coord { x: current.x - dy, y: current.y - dx },
-            '─' => Coord { x: current.x + dx, y: current.y + dy },
-            '┐' => Coord { x: current.x - dy, y: current.y - dx },
-            '┘' => Coord { x: current.x + dy, y: current.y + dx },
-            '│' => Coord { x: current.x + dx, y: current.y + dy },
-            _ => current,
-        };
+        prev.x = current.x;
+        prev.y = current.y;
 
-        prev = current;
-        current = next;
+        match &map[current.x as usize][current.y as usize] {
+            '┌' => {
+                current.x = current.x + dy;
+                current.y = current.y + dx
+            }
+            '└' => {
+                current.x = current.x - dy;
+                current.y = current.y - dx
+            }
+            '─' => {
+                current.x = current.x + dx;
+                current.y = current.y + dy
+            }
+            '┐' => {
+                current.x = current.x - dy;
+                current.y = current.y - dx
+            }
+            '┘' => {
+                current.x = current.x + dy;
+                current.y = current.y + dx
+            }
+            '│' => {
+                current.x = current.x + dx;
+                current.y = current.y + dy
+            }
+            _ => panic!("Unknown char {}", &map[current.x as usize][current.y as usize]),
+        };
     }
     let ans_1 = ans_1 / 2;
 
     let mut ans_2 = 0;
     for y in 0..len {
-        let mut loop_passings: i32 = 0;
+        let mut loop_passings = 0;
         let mut from_below = false;
         for x in 0..len {
-            let on_loop = part_of_loop.get(&(x as i32, y as i32)).is_some();
-
+            let on_loop = part_of_loop[x][y];
             if on_loop {
                 match &map[x][y] {
                     '┐' if !from_below => loop_passings = loop_passings + 1,
@@ -99,6 +86,53 @@ pub fn run(input: String) -> (usize, usize) {
     (ans_1, ans_2)
 }
 
+#[derive(PartialEq, Clone, Copy)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+fn find_start_coord(map: &mut Vec<Vec<char>>) -> Coord {
+    map.iter().enumerate().find_map(|(x, line)| {
+        line.iter().enumerate().find_map(|(y, tile)| {
+            if *tile == 'S' {
+                Some(Coord { x: x as i32, y: y as i32 })
+            } else {
+                None
+            }
+        })
+    }).unwrap()
+}
+
+fn get_start_tile_type(map: &Vec<Vec<char>>, start: &Coord) -> char {
+    let left = map[(start.x - 1) as usize][start.y as usize];
+    let right = map[(start.x + 1) as usize][start.y as usize];
+    let up = map[start.x as usize][(start.y + 1) as usize];
+    let left: bool = left == '─' || left == '└' || left == '┌';
+    let right: bool = right == '─' || right == '┐' || right == '┘';
+    let up: bool = up == '│' || up == '┐' || up == '┘';
+
+    let start_char = match (left, right, up) {
+        (false, true, false) => '┌',
+        (false, true, true) => '└',
+        (true, true, false) => '─',
+        (true, false, false) => '┐',
+        (true, false, true) => '┘',
+        (false, false, true) => '│',
+        _ => panic!("Unknown start char"),
+    };
+    start_char
+}
+
+fn get_first_step(start: Coord, start_char: char) -> Coord {
+    let first = match start_char {
+        '┌' | '└' | '─' => Coord { x: start.x + 1, y: start.y },
+        '┐' | '┘' => Coord { x: start.x - 1, y: start.y },
+        _ => Coord { x: start.x, y: start.y + 1 },
+    };
+    first
+}
+
 fn clean(input: String) -> String {
     let input = input
         .replace("F", "┌")
@@ -108,10 +142,4 @@ fn clean(input: String) -> String {
         .replace("J", "┘")
         .replace("|", "│");
     input
-}
-
-#[derive(PartialEq, Clone, Copy)]
-struct Coord {
-    x: i32,
-    y: i32,
 }
