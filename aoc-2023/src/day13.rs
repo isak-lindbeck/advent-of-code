9351e3dcfcf1200a91055660a321a7e0ca08a855
Isak Lindbeck <isak.lindbeck@atrazeneca.com>
2023-12-13 16:04:14 +0100

    Day 13 cleanup

--- aoc-2023/src/day13.rs
+++ aoc-2023/src/day13.rs
@@ -5,45 +5,39 @@ pub fn run(input: String) -> (usize, usize) {
         let height = pattern.lines().count();
         let width = pattern.lines().next().unwrap().len();
         let mut map: Vec<Vec<char>> = vec![vec!['.'; height]; width];
-        let mut rotated_map: Vec<Vec<char>> = vec![vec!['.'; width]; height];
+        let mut map_rotated: Vec<Vec<char>> = vec![vec!['.'; width]; height];
 
         pattern.lines().enumerate().for_each(|(y, line)| {
             line.chars().enumerate().for_each(|(x, c)| {
-                map[x][height - y - 1] = c;
-                rotated_map[height - y - 1][x] = c
+                map[x][y] = c;
+                map_rotated[y][x] = c;
             });
         });
 
-        let spaces = width - 1;
-        for x in 0..spaces {
-            let mut errors: usize = 0;
-            (0..=x).rev().zip(x + 1..=spaces).for_each(|(l1, l2)| {
-                let i: usize = map[l1].iter().zip(map[l2].iter()).filter(|(a, b)| a != b).count();
-                errors += i;
-            });
-            if errors == 0 {
-                ans_1 += x + 1;
-            }
-            if errors == 1 {
-                ans_2 += x + 1;
-            }
-        }
-
-        let spaces = height - 1;
-        for y in 0..spaces {
-            let mut errors: usize = 0;
-            (0..=y).rev().zip(y + 1..=spaces).for_each(|(y1, y2)| {
-                let i: usize = rotated_map[y1].iter().zip(rotated_map[y2].iter()).filter(|(a, b)| a != b).count();
-                errors += i;
-            });
-            if errors == 0 {
-                ans_1 += (height - y - 1) * 100;
-            }
-            if errors == 1 {
-                ans_2 += (height - y - 1) * 100;
-            }
-        }
+        let res_1: (usize, usize) = do_the_thing(width, height, &map);
+        let res_2: (usize, usize) = do_the_thing(height, width, &map_rotated);
+        ans_1 += res_1.0 + res_2.0 * 100;
+        ans_2 += res_1.1 + res_2.1 * 100;
     });
 
     (ans_1, ans_2)
+}
+
+fn do_the_thing(width: usize, height: usize, map: &Vec<Vec<char>>) -> (usize, usize) {
+    let mut sum_1 = 0;
+    let mut sum_2 = 0;
+    for x in 0..width - 1 {
+        let mut errors: usize = 0;
+        (0..=x).rev().zip(x + 1..width).for_each(|(x1, x2)| {
+            let differences: usize = (0..height).filter(|y| map[x1][*y] != map[x2][*y]).count();
+            errors += differences;
+        });
+        if errors == 0 {
+            sum_1 += x + 1;
+        }
+        if errors == 1 {
+            sum_2 += x + 1;
+        }
+    }
+    (sum_1, sum_2)
 }
