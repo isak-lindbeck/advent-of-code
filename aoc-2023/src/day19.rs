66e96d99b412192380ff6b2f81099a8e7309eb05
Isak Lindbeck <isak.lindbeck@atrazeneca.com>
2023-12-19 21:48:24 +0100

    Day 19 part 2

--- aoc-2023/src/day19.rs
+++ aoc-2023/src/day19.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use crate::day19::Op::*;
 
 pub fn run(input: String) -> (usize, usize) {
     let split: Vec<&str> = input.split("\n\n").collect();
@@ -13,53 +14,148 @@ pub fn run(input: String) -> (usize, usize) {
         Xmas { x: nums[0], m: nums[1], a: nums[2], s: nums[3] }
     }).collect();
 
-    let mut work_flows: HashMap<&str, Vec<Box<dyn Fn(&Xmas) -> Option<String>>>> = HashMap::new();
+    let mut work_flows: HashMap<&str, WorkFlow> = HashMap::new();
 
     split[0].lines().map(|l| {
         let (name, s) = l[..l.len() - 1].split_once("{").unwrap();
-        let instruction: Vec<Box<dyn Fn(&Xmas) -> Option<String>>> = s.split(",")
+        let instruction: Vec<Instruction> = s.split(",")
             .map(|s| {
-                let instr: Box<dyn Fn(&Xmas) -> Option<String>> = match s {
-                    "A" => Box::new(|_: &Xmas| -> Option<String> { Some("A".to_string()) }),
-                    "R" => Box::new(|_: &Xmas| -> Option<String> { Some("R".to_string()) }),
-                    _ if s.contains("<") => { parse_comparison(s, "<") }
-                    _ if s.contains(">") => { parse_comparison(s, ">") }
-                    _ => Box::new(|_: &Xmas| -> Option<String> { Some(s.to_string()) }),
-                };
-                instr
+                match s {
+                    "A" => Instruction { letter: "x", op: Gt, val: 0, result: "A" },
+                    "R" => Instruction { letter: "x", op: Gt, val: 0, result: "R" },
+                    _ if s.contains("<") => { parse_instruction(s, "<") }
+                    _ if s.contains(">") => { parse_instruction(s, ">") }
+                    _ => Instruction { letter: "x", op: Gt, val: 0, result: s },
+                }
             })
             .collect();
-        (name, instruction)
+        (name, WorkFlow { instructions: instruction })
     }).for_each(|(name, instruction)| { work_flows.insert(name, instruction); });
 
 
     let mut ans_1 = 0;
     for xmas in xmas_list {
-        let mut wf: &Vec<Box<dyn Fn(&Xmas) -> Option<String>>> = work_flows.get("in").unwrap();
-
-        let mut option = None;
+        let mut wf: &WorkFlow = work_flows.get("in").unwrap();
         let mut i = 0;
-        while option.is_none() {
-            option = wf[i](&xmas);
+        let mut done = false;
+        while !done {
+            let inst: &Instruction = &wf.instructions[i];
             i += 1;
-            if let Some(ref x) = option {
-                match x.as_str() {
-                    "A" => { ans_1 += xmas.x + xmas.m + xmas.a + xmas.s; }
-                    "R" => {  }
+            let value = match inst.letter {
+                "x" => { xmas.x }
+                "m" => { xmas.m }
+                "a" => { xmas.a }
+                "s" => { xmas.s }
+                _ => { panic!("Invalid instruction!") }
+            };
+
+            let passed = match inst.op {
+                Gt => value > inst.val,
+                Lt => value < inst.val,
+            };
+
+            if passed {
+                match inst.result {
+                    "A" => {
+                        ans_1 += xmas.x + xmas.m + xmas.a + xmas.s;
+                        done = true
+                    }
+                    "R" => { done = true }
                     _ => {
-                        wf = work_flows.get(x.as_str()).unwrap();
+                        wf = work_flows.get(inst.result).unwrap();
                         i = 0;
-                        option = None;
                     }
                 }
             }
         }
     }
 
-    (ans_1, 0)
+    let mut ans_2 = 0;
+    let mut work_queue: Vec<(&str, usize, ReXmas)> = Vec::new();
+    work_queue.push(("in", 0, ReXmas { x: Range { start: 1, end: 4000 }, m: Range { start: 1, end: 4000 }, a: Range { start: 1, end: 4000 }, s: Range { start: 1, end: 4000 } }));
+    while let Some((wf_name, wf_idx, rexmas)) = work_queue.pop() {
+        let wf: &WorkFlow = work_flows.get(wf_name).unwrap();
+
+        let inst: &Instruction = &wf.instructions[wf_idx];
+        let range = match inst.letter {
+            "x" => { rexmas.x }
+            "m" => { rexmas.m }
+            "a" => { rexmas.a }
+            "s" => { rexmas.s }
+            _ => { panic!("Invalid instruction!") }
+        };
+
+        let mut maybe_split_ranges: Vec<Range> = Vec::new();
+
+        if range.start < inst.val && inst.val < range.end {
+            if inst.op == Gt {
+                maybe_split_ranges.push(Range { start: range.start, end: inst.val });
+                maybe_split_ranges.push(Range { start: inst.val + 1, end: range.end });
+            } else {
+                maybe_split_ranges.push(Range { start: range.start, end: inst.val - 1 });
+                maybe_split_ranges.push(Range { start: inst.val, end: range.end });
+            }
+        } else {
+            maybe_split_ranges.push(range);
+        }
+
+        for range in maybe_split_ranges {
+
+            let mut new_rexmas = rexmas.clone();
+            match inst.letter {
+                "x" => { new_rexmas.x = range }
+                "m" => { new_rexmas.m = range }
+                "a" => { new_rexmas.a = range }
+                "s" => { new_rexmas.s = range }
+                _ => { panic!("Invalid instruction!") }
+            }
+
+            let passed = match inst.op {
+                Gt => range.start > inst.val,
+                Lt => range.end < inst.val,
+            };
+            if passed {
+                match inst.result {
+                    "A" => {
+                        let x_range = new_rexmas.x.end - new_rexmas.x.start + 1;
+                        let m_range = new_rexmas.m.end - new_rexmas.m.start + 1;
+                        let a_range = new_rexmas.a.end - new_rexmas.a.start + 1;
+                        let s_range = new_rexmas.s.end - new_rexmas.s.start + 1;
+
+                        ans_2 += x_range * m_range * a_range * s_range;
+                    }
+                    "R" => {  }
+                    _ => {
+                        let item = (inst.result, 0, new_rexmas);
+                        work_queue.push(item);
+                    }
+                }
+            } else {
+                let item = (wf_name, wf_idx + 1, new_rexmas);
+                work_queue.push(item);
+            }
+        }
+    }
+    (ans_1, ans_2)
 }
 
-#[derive(Debug)]
+#[derive(PartialEq)]
+enum Op {
+    Gt,
+    Lt,
+}
+
+struct Instruction<'a> {
+    letter: &'a str,
+    op: Op,
+    val: usize,
+    result: &'a str,
+}
+
+struct WorkFlow<'a> {
+    instructions: Vec<Instruction<'a>>,
+}
+
 struct Xmas {
     x: usize,
     m: usize,
@@ -67,23 +163,27 @@ struct Xmas {
     s: usize,
 }
 
-fn parse_comparison<'a>(s: &'a str, comparison: &'a str) -> Box<dyn Fn(&Xmas) -> Option<String> + 'a> {
+#[derive(Clone,Copy)]
+struct Range {
+    start: usize,
+    end: usize,
+}
+#[derive(Clone,Copy)]
+struct ReXmas {
+    x: Range,
+    m: Range,
+    a: Range,
+    s: Range,
+}
+
+fn parse_instruction<'a>(s: &'a str, comparison: &'a str) -> Instruction<'a> {
     let (letter, x) = s.split_once(comparison).unwrap();
-    let (num, result) = x.split_once(":").unwrap();
-    let num: usize = num.parse::<usize>().unwrap();
-
-    let resolve: Box<dyn Fn(usize) -> bool> = match comparison {
-        "<" => Box::new(move |v: usize| -> bool { v < num }),
-        ">" => Box::new(move |v: usize| -> bool { v > num }),
+    let (val, result) = x.split_once(":").unwrap();
+    let val: usize = val.parse::<usize>().unwrap();
+    let op = match comparison {
+        "<" => Lt,
+        ">" => Gt,
         _ => panic!("Invalid instruction: {}", s),
     };
-
-    let get_value: Box<dyn Fn(&Xmas) -> usize> = match letter {
-        "x" => Box::new(move |xmas: &Xmas| -> usize { xmas.x }),
-        "m" => Box::new(move |xmas: &Xmas| -> usize { xmas.m }),
-        "a" => Box::new(move |xmas: &Xmas| -> usize { xmas.a }),
-        "s" => Box::new(move |xmas: &Xmas| -> usize { xmas.s }),
-        _ => panic!("Invalid instruction: {}", s),
-    };
-    Box::new(move |xmas: &Xmas| -> Option<String> { if resolve(get_value(xmas)) { Some(result.to_string()) } else { None } })
+    Instruction { letter, op, val, result }
 }
