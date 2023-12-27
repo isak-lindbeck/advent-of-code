6c9fcd0804a86b7f3b0d23a6581179cd42b5986d
Isak Lindbeck <isak.lindbeck@atrazeneca.com>
2023-12-27 15:27:08 +0100

    Day 20 part 2

--- aoc-2023/src/day20.rs
+++ aoc-2023/src/day20.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
+
+use num::integer::lcm;
+
 use Module::{Broadcaster, FlipFlop};
+
 use crate::day20::Module::Conjunction;
 use crate::day20::Signal::{High, Low};
 use crate::day20::State::{Off, On};
@@ -48,92 +52,20 @@ pub fn run(input: String) -> (usize, usize) {
         }
     }
 
-    // println!("Modules: {:?}", modules);
-
     let mut sum_low = 0;
     let mut sum_high = 0;
     let button_id = ModuleId("button".into());
     let broadcaster_id = ModuleId("broadcaster".into());
 
-    // for i in 0..1000 {
-    //     let mut work_queue: Vec<(Signal, &ModuleId, &ModuleId)> = Vec::new();
-    //     work_queue.push((Low, &button_id, &broadcaster_id));
-    //     while !work_queue.is_empty() {
-    //         let (signal, source_id, target_id) = work_queue.remove(0);
-    //         // println!("{:?} -{:?}-> {:?} : {} * {}", source_id.0, signal, target_id.0, sum_low, sum_high);
-    //         match signal {
-    //             High => { sum_high += 1 }
-    //             Low => { sum_low += 1 }
-    //         }
-    //
-    //         if target_id.0 == "rx" {
-    //             println!("HEJ!")
-    //         }
-    //
-    //         let target_module: Option<&Module> = modules.get(target_id);
-    //         match target_module {
-    //             Some(Broadcaster(t)) => { t.iter().for_each(|next_target_id| { work_queue.push((Low, target_id, next_target_id)) }) }
-    //             Some(FlipFlop(t)) => {
-    //                 let current_state: &State = flip_flop_states.get(target_id).unwrap_or(&Off);
-    //                 if signal == Low {
-    //                     let (next_state, next_signal) = match current_state {
-    //                         On => (Off, Low),
-    //                         Off => (On, High),
-    //                     };
-    //                     flip_flop_states.insert(target_id, next_state);
-    //                     t.iter().for_each(|next_target_id| { work_queue.push((next_signal, target_id, next_target_id)) })
-    //                 }
-    //             }
-    //             Some(Conjunction(t)) => {
-    //                 conjunction_states.insert((source_id, target_id), signal);
-    //                 let all_high_in_mem = conjunction_states.iter()
-    //                     .filter(|((_, id), _)| *id == target_id)
-    //                     .all(|((_, _), signal)| signal == &High);
-    //                 let next_signal = if all_high_in_mem {
-    //                     Low
-    //                 } else {
-    //                     High
-    //                 };
-    //                 t.iter().for_each(|next_target_id| { work_queue.push((next_signal, target_id, next_target_id)) })
-    //             }
-    //             _ => {}
-    //         };
-    //     }
-    // }
-
-    let ans_1 = sum_low * sum_high;
-
-    let mut button_presses = 0;
-    let mut rx_low_signals = 0;
-    let mut _rx_high_signals = 0;
-    while !(rx_low_signals >= 1) {
-        rx_low_signals = 0;
-        _rx_high_signals = 0;
+    for i in 0..1000 {
         let mut work_queue: Vec<(Signal, &ModuleId, &ModuleId)> = Vec::new();
         work_queue.push((Low, &button_id, &broadcaster_id));
-        button_presses += 1;
-        if button_presses % 1_000_000 == 0 {
-            println!("{:?}", button_presses);
-        }
         while !work_queue.is_empty() {
             let (signal, source_id, target_id) = work_queue.remove(0);
-            // println!("{:?} -{:?}-> {:?}", source_id.0, signal, target_id.0);
             match signal {
-                High => {
-                    if target_id.0 == "output" {
-                        _rx_high_signals += 1;
-                    }
-                    sum_high += 1
-                }
-                Low => {
-                    if target_id.0 == "output" {
-                        rx_low_signals += 1;
-                    }
-                    sum_low += 1
-                }
+                High => { sum_high += 1 }
+                Low => { sum_low += 1 }
             }
-
-
 
             let target_module: Option<&Module> = modules.get(target_id);
             match target_module {
@@ -165,7 +97,79 @@ pub fn run(input: String) -> (usize, usize) {
             };
         }
     }
-    let ans_2 = button_presses;
+
+    let mut prev_button_presses: HashMap<&ModuleId, usize> = HashMap::new();
+    let mut cycles: HashMap<&ModuleId, usize> = HashMap::new();
+
+    let ans_1 = sum_low * sum_high;
+
+    let mut button_presses = 0;
+    let mut done = false;
+    while !done {
+        let mut work_queue: Vec<(Signal, &ModuleId, &ModuleId)> = Vec::new();
+        work_queue.push((Low, &button_id, &broadcaster_id));
+        button_presses += 1;
+        if button_presses % 1_000_000 == 0 {
+            println!("{:?}", button_presses);
+        }
+        while !work_queue.is_empty() {
+            let (signal, source_id, target_id) = work_queue.remove(0);
+            // println!("{:?} -{:?}-> {:?}", source_id.0, signal, target_id.0);
+            let target_module: Option<&Module> = modules.get(target_id);
+            match target_module {
+                Some(Broadcaster(t)) => { t.iter().for_each(|next_target_id| { work_queue.push((Low, target_id, next_target_id)) }) }
+                Some(FlipFlop(t)) => {
+                    let current_state: &State = flip_flop_states.get(target_id).unwrap_or(&Off);
+                    if signal == Low {
+                        let (next_state, next_signal) = match current_state {
+                            On => (Off, Low),
+                            Off => (On, High),
+                        };
+                        flip_flop_states.insert(target_id, next_state);
+                        t.iter().for_each(|next_target_id| { work_queue.push((next_signal, target_id, next_target_id)) })
+                    }
+                }
+                Some(Conjunction(t)) => {
+                    conjunction_states.insert((source_id, target_id), signal);
+                    let all_high_in_mem = conjunction_states.iter()
+                        .filter(|((_, id), _)| *id == target_id)
+                        .all(|((_, _), signal)| signal == &High);
+                    let next_signal = if all_high_in_mem {
+                        Low
+                    } else {
+                        High
+                    };
+
+                    if target_id.0 == "tg" && signal == High {
+                        println!("{} {:?} -{:?}-> {:?}", button_presses, source_id, signal, target_id);
+                        let x: Vec<(String, Signal)> = conjunction_states.iter()
+                            .filter(|((_, id), _)| *id == target_id)
+                            .map(|((s, t), signal)| (s.0.clone(), *signal))
+                            .collect();
+                        let old: Option<usize> = prev_button_presses.insert(source_id, button_presses);
+                        if let Some(old) = old {
+                            println!("Cycle {} detected?", button_presses - old);
+                            cycles.insert(source_id, button_presses - old);
+                            if cycles.len() == x.len() {
+                                done = true;
+                            }
+                        }
+                        println!("{:?}", x);
+                        println!()
+                    }
+
+                    t.iter().for_each(|next_target_id| { work_queue.push((next_signal, target_id, next_target_id)) })
+                }
+                _ => {}
+            };
+        }
+    }
+
+    cycles.iter().for_each(|c| println!("{:?}", c.1));
+
+    let ans_2 = cycles.iter().map(|c| *c.1)
+        .reduce(lcm)
+        .unwrap();
 
     (ans_1, ans_2)
 }
