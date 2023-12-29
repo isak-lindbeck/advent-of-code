8ea72a66af39ed61ca1a95bc437aa36cd7835256
Isak Lindbeck <isak.lindbeck@atrazeneca.com>
2023-12-30 00:42:32 +0100

    Day 23 part 2

--- aoc-2023/src/day23.rs
+++ aoc-2023/src/day23.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use priority_queue::PriorityQueue;
 
 pub fn run(input: String) -> (usize, usize) {
@@ -9,65 +10,134 @@ pub fn run(input: String) -> (usize, usize) {
         });
     });
 
-    let x = calculate_djikstra(&map);
+    let start = (1, 0);
+    let mut graph: Vec<Vec<(usize, usize)>> = Vec::new();
+    let mut nodes: HashMap<(usize, usize), usize> = HashMap::new();
 
-    (x, 0)
-}
+    let start_idx = 0;
+    nodes.insert(start, start_idx);
+    graph.push(Vec::new());
+    let end_idx = 1;
+    nodes.insert((side - 2, side - 1), end_idx);
+    graph.push(Vec::new());
 
-
-fn calculate_djikstra(map: &Vec<Vec<char>>) -> usize {
-    let side = map.len();
-    let mut steps: Vec<Vec<usize>> = vec![vec![0; side]; side];
-    steps[1][0] = 0;
-    let mut queue: PriorityQueue<(usize, usize, Vec<(usize, usize)>), usize> = PriorityQueue::new();
-    queue.push((1, 0, vec![(1,0)]), 0);
-
-    while let Some((node, _)) = queue.pop() {
-        let (x, y, visited) = node;
-        // if x == side - 1 && y == side - 1 {
-        //     break;
-        // }
-        let neighbors = get_neighbours(&map, (x, y, visited.clone()));
-        for neighbour in neighbors {
-            let (n_x, n_y) = neighbour;
-            let new_steps = steps[x][y] + 1;
-            let prev_steps = steps[n_x][n_y];
-            if new_steps > prev_steps {
-                steps[n_x][n_y] = new_steps;
-                let mut vis = visited.clone();
-                vis.push((n_x, n_y));
-                queue.push((n_x, n_y, vis), new_steps);
+    for y in 1..side - 1 {
+        for x in 1..side - 1 {
+            let v = [map[x - 1][y], map[x + 1][y], map[x][y - 1], map[x][y + 1]];
+            if (map[x - 1][y] == '>' || map[x][y - 1] == 'v') && (map[x + 1][y] == '>' || map[x][y + 1] == 'v') {
+                let node_idx = nodes.len();
+                nodes.insert((x, y), node_idx);
+                graph.push(Vec::new());
             }
         }
     }
-    let steps = steps[side - 2][side - 1];
-    steps
+
+    nodes.iter().for_each(|(&(x, y), &node_idx)| {
+        let neighbours = get_node_neighbours(&map, (x, y));
+        neighbours.iter().for_each(|&n| {
+            let mut prev = (x, y);
+            let mut current = n;
+            let mut count = 1;
+            while !nodes.contains_key(&current) {
+                let next = walk(&map, current, prev);
+                prev = current;
+                current = next;
+                count += 1;
+            }
+            let c_idx = nodes.get(&current).unwrap();
+            graph[node_idx].push((*c_idx, count));
+            // println!("{x},{y} -> {},{}: ({count})", current.0, current.1);
+        });
+    });
+
+    // println!("nodes: {:?}", nodes);
+    // println!();
+    // println!("{:?}", graph);
+    // println!();
+
+    let ans_1 = calculate_djikstra(&graph);
+
+
+    (ans_1, 0)
 }
 
-fn get_neighbours(map: &Vec<Vec<char>>, node: (usize, usize, Vec<(usize, usize)>)) -> Vec<(usize, usize)> {
-    let (x, y, visited) = node;
-    let c = map[x][y];
+fn get_node_neighbours(map: &Vec<Vec<char>>, current: (usize, usize)) -> Vec<(usize, usize)> {
+    let (x, y) = current;
     let mut neighbors: Vec<(usize, usize)> = Vec::new();
 
-    if x > 0 && (c == '.' || c == '<') {
-        if !visited.contains(&(x - 1, y)) && map[x - 1][y] != '#' {
+    if x > 0 {
+        if map[x - 1][y] != '#' {
             neighbors.push((x - 1, y));
         }
     }
-    if x < map.len() - 1 && (c == '.' || c == '>') {
-        if !visited.contains(&(x + 1, y)) && map[x + 1][y] != '#' {
+    if x < map.len() - 1 {
+        if map[x + 1][y] != '#' {
             neighbors.push((x + 1, y));
         }
     }
-    if y > 0 && (c == '.' || c == '^') {
-        if !visited.contains(&(x, y - 1)) && map[x][y - 1] != '#' {
+    if y > 0 {
+        if map[x][y - 1] != '#' {
             neighbors.push((x, y - 1));
         }
     }
-    if y < map.len() - 1 && (c == '.' || c == 'v') {
-        if !visited.contains(&(x, y + 1)) && map[x][y + 1] != '#' {
+    if y < map.len() - 1 {
+        if map[x][y + 1] != '#' {
             neighbors.push((x, y + 1));
         }
     }
     neighbors
+}
+
+fn get_node_neighbours_slopes(map: &Vec<Vec<char>>, current: (usize, usize)) -> Vec<(usize, usize)> {
+    let (x, y) = current;
+    let mut neighbors: Vec<(usize, usize)> = Vec::new();
+    if x < map.len() - 1 {
+        if map[x + 1][y] != '#' {
+            neighbors.push((x + 1, y));
+        }
+    }
+    if y < map.len() - 1 {
+        if map[x][y + 1] != '#' {
+            neighbors.push((x, y + 1));
+        }
+    }
+    neighbors
+}
+
+fn walk(map: &Vec<Vec<char>>, current: (usize, usize), prev: (usize, usize)) -> (usize, usize) {
+    let (x, y) = current;
+    if !(prev == (x - 1, y)) && map[x - 1][y] != '#' {
+        return (x - 1, y);
+    } else if !(prev == (x + 1, y)) && map[x + 1][y] != '#' {
+        return (x + 1, y);
+    } else if !(prev == (x, y - 1)) && map[x][y - 1] != '#' {
+        return (x, y - 1);
+    } else {
+        return (x, y + 1);
+    }
+}
+
+fn calculate_djikstra(graph: &Vec<Vec<(usize, usize)>>) -> usize {
+    let side = graph.len();
+    let mut distance: Vec<usize> = vec![0; side];
+
+    let mut queue: PriorityQueue<(usize, Vec<usize>), usize> = PriorityQueue::new();
+    queue.push((0, Vec::new()), 0);
+
+    while let Some(((node_idx, visited), prev_cost)) = queue.pop() {
+        for &(neighbour_idx, cost) in graph[node_idx].iter() {
+            if visited.contains(&neighbour_idx) {
+                continue;
+            }
+            let new_cost = prev_cost + cost;
+            if new_cost > distance[neighbour_idx] {
+                distance[neighbour_idx] = new_cost;
+            }
+            let mut vis = visited.clone();
+            vis.push(neighbour_idx);
+            queue.push((neighbour_idx, vis), new_cost);
+        }
+    }
+    let end_cost = distance[1];
+    end_cost
 }
